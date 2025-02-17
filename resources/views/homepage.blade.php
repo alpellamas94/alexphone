bbd53167612d52d43bf3d272a914c36e3d755cbb
Alex Pérez <alpellamas@gmail.com>
2025-02-17 20:47:20 +0100

    layout base

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+
+@section('title_page')
+Home
+@endsection
+
+@section('content')
+    <div class="mdl-header show">
+        <a href="https://www.alexphone.es/" target="_blank" class="m-logo">
+            <img src="{{ asset('/imgs/alexphone.svg') }}" alt="Logo alexphone">
+        </a>
+
+        <a href="#" target="_self" class="m-carrito">
+            <img src="{{ asset('/imgs/carrito.svg') }}" alt="Carrito">
+            <span>2</span>
+        </a>
+    </div>
+
+    <div class="mdl-hero"></div>
+@endsection
