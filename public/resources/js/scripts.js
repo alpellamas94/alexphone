4162ee1666f5a145d0cb86f22bd92f0fd2fa5d06
Alex Pérez <alpellamas@gmail.com>
2025-02-21 01:07:37 +0100

    small changes style views

--- public/resources/js/scripts.js
+++ public/resources/js/scripts.js
@@ -165,7 +165,7 @@ $(function () {
                     
                     setTimeout(() => {
                         $("#message-cart").removeClass("active");
-                    }, 2000);
+                    }, 1000);
 
                     $("#m-cart").load(window.location.href + " #m-cart > *");
                 },
