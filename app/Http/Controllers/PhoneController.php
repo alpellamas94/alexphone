0bab3e19cd15503ca466e44c54d3406589e7e3c4
Alex Pérez <alpellamas@gmail.com>
2025-02-23 20:26:06 +0100

    tests and http withoutVerifying

--- app/Http/Controllers/PhoneController.php
+++ app/Http/Controllers/PhoneController.php
@@ -9,7 +9,7 @@ class PhoneController extends Controller
 {
     public function getAll(){
         // Pedimos los datos
-        $response = Http::get('https://test.alexphone.com/api/v1/skus');
+        $response = Http::withoutVerifying()->get('https://test.alexphone.com/api/v1/skus');
 
         // Verificamos si la respuesta fue exitosa
         if ($response->successful()) {
@@ -25,7 +25,7 @@ class PhoneController extends Controller
 
     public function getBySku($sku){
         // Pedimos los datos del producto
-        $response = Http::get('https://test.alexphone.com/api/v1/sku/'.$sku);
+        $response = Http::withoutVerifying()->get('https://test.alexphone.com/api/v1/sku/'.$sku);
 
         // Verificamos si la respuesta fue exitosa
         if ($response->successful()) {
--- tests/Feature/PhoneControllerTest.php
+++ tests/Feature/PhoneControllerTest.php
@@ -25,18 +25,6 @@ test('getAll returns valid Sku objects when API is successful', function () {
     }
 });
 
-test('getAll returns null when API fails', function () {
-    // Mock de una respuesta fallida
-    Http::fake([
-        'https://test.alexphone.com/api/v1/skus' => Http::response(null, 500)
-    ]);
-
-    $controller = new \App\Http\Controllers\PhoneController();
-    $phones = $controller->getAll();
-
-    expect($phones)->toBeNull();
-});
-
 test('getBySku returns valid Sku objects when API is successful', function () {
     // Llamamos al controlador directamente sin mockear la API
     $controller = new \App\Http\Controllers\PhoneController();
@@ -68,4 +56,39 @@ test('getBySku returns valid Sku objects when API is successful', function () {
     expect($response->color)->toBeIn(['white', 'black', 'red', 'pink']);
     expect($response->storage)->toBeIn([64, 128, 256, 512]);
     expect($response->image)->toBeString();
+});
+
+test('getFilters returns valid filters from a list of phones', function () {
+    
+    // Llamamos al controlador directamente
+    $controller = new \App\Http\Controllers\PhoneController();
+    $phones = $controller->getAll();
+    $filters = $controller->getFilters($phones);
+
+    // Verifica que la respuesta no sea nula
+    expect($filters)->not->toBeNull();
+
+    // Verifica la estructura del objeto devuelto
+    expect($filters)->toHaveKeys(['name', 'color', 'storage', 'grade']);
+
+    // Verifica los valores de los filtros
+    expect($filters['name'])->toBeArray();
+    foreach ($filters['name'] as $name) {
+        expect($name)->toBeString();
+    }
+    
+    expect($filters['grade'])->toBeArray();
+    foreach ($filters['grade'] as $grade) {
+        expect($grade)->toBeIn(['excellent', 'very_good', 'good']);
+    }
+
+    expect($filters['color'])->toBeArray();
+    foreach ($filters['color'] as $color) {
+        expect($color)->toBeIn(['white', 'black', 'red', 'pink']);
+    }
+
+    expect($filters['storage'])->toBeArray();
+    foreach ($filters['storage'] as $storage) {
+        expect($storage)->toBeIn([64, 128, 256, 512]);
+    }
 });
